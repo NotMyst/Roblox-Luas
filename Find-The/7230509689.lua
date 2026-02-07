c1d3e75ace2ee9bcb005f173773abe9466f524a6
Nitro4k <168119457+NotMyst@users.noreply.github.com>
2026-02-08 00:48:13 +0100

    Create 7230509689.lua

---
+++ Find-The/7230509689.lua
@@ -0,0 +1,16 @@
+-- https://www.roblox.com/games/139627733413055/Find-The-Seals
+
+local transmitters = {}
+for _, v in next, workspace.Collectibles:GetChildren() do
+    local interest = v:FindFirstChild("TouchInterest", true)
+    if interest then
+        firetouchinterest(game.Players.LocalPlayer.Character.PrimaryPart, interest.Parent, 1)
+        table.insert(transmitters, interest)
+    end
+end
+
+task.wait(1)
+
+for _, v in next, transmitters do
+    firetouchinterest(game.Players.LocalPlayer.Character.PrimaryPart, v.Parent, 0)
+end
