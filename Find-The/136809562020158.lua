52913655893053634e23eed90289bc703608440f
Nitro4k <168119457+NotMyst@users.noreply.github.com>
2026-02-08 00:12:22 +0100

    Create 136809562020158.lua

---
+++ Find-The/136809562020158.lua
@@ -0,0 +1,39 @@
+for _, v in next, workspace.Rocks:GetChildren() do
+    if v:FindFirstChildOfClass("TouchTransmitter") then
+        firetouchinterest(game.Players.LocalPlayer.Character.PrimaryPart, v, 1)
+    end
+end
+
+local eightball = workspace:FindFirstChild("8Ball")
+if eightball and eightball:FindFirstChildOfClass("TouchTransmitter") then
+    firetouchinterest(game.Players.LocalPlayer.Character.PrimaryPart, eightball, 1)
+end
+
+local cavediver = workspace.Volcano["Cave Diver Rock"]
+if cavediver and cavediver:FindFirstChildOfClass("TouchTransmitter") then
+    firetouchinterest(game.Players.LocalPlayer.Character.PrimaryPart, cavediver, 1)
+end
+
+local lavarock = workspace.Volcano["Lava Rock"]
+if lavarock and lavarock:FindFirstChildOfClass("TouchTransmitter") then
+    firetouchinterest(game.Players.LocalPlayer.Character.PrimaryPart, lavarock, 1)
+end
+
+task.wait(1)
+for _, v in next, workspace.Rocks:GetChildren() do
+    if v:FindFirstChildOfClass("TouchTransmitter") then
+        firetouchinterest(game.Players.LocalPlayer.Character.PrimaryPart, v, 0)
+    end
+end
+
+if eightball and eightball:FindFirstChildOfClass("TouchTransmitter") then
+    firetouchinterest(game.Players.LocalPlayer.Character.PrimaryPart, eightball, 0)
+end
+
+if cavediver and cavediver:FindFirstChildOfClass("TouchTransmitter") then
+    firetouchinterest(game.Players.LocalPlayer.Character.PrimaryPart, cavediver, 0)
+end
+
+if lavarock and lavarock:FindFirstChildOfClass("TouchTransmitter") then
+    firetouchinterest(game.Players.LocalPlayer.Character.PrimaryPart, lavarock, 0)
+end
