9f3f8007652df0aaf6a5a488846d2227ef8e5dfb
Nitro4k <168119457+NotMyst@users.noreply.github.com>
2026-02-08 14:41:18 +0100

    Create 7709099852.lua

---
+++ Find-The/7709099852.lua
@@ -0,0 +1,18 @@
+-- https://www.roblox.com/games/105834035548197/Find-the-Gubbys
+
+local transmitters = {}
+for _, v in next, workspace.ToFind:GetChildren() do
+    local interest = v:FindFirstChild("TouchInterest")
+    if interest then
+        if not game.Players.LocalPlayer.ToFindFolder[v.Name].Value then
+            firetouchinterest(game.Players.LocalPlayer.Character.PrimaryPart, v, 1)
+            table.insert(transmitters, interest)
+        end
+    end
+end
+
+task.wait(1)
+
+for _, v in next, transmitters do
+    firetouchinterest(game.Players.LocalPlayer.Character.PrimaryPart, v.Parent, 0)
+end
